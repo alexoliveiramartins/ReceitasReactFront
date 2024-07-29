4e6eca903b0a9115bb2318deabaecbf3ce3aa158
MarcoAntonioUFG <146884614+MarcoAntonioUFG@users.noreply.github.com>
2024-07-29 17:32:45 -0300

    Update Inicio.jsx

--- src/components/MainPage/Inicio.jsx
+++ src/components/MainPage/Inicio.jsx
@@ -8,7 +8,6 @@ export function Inicio(){
         <h1 className="text-5xl flex flex-col justify-center items-center font-poppings">Explorar Receitas</h1>
         <Tag></Tag>
         <div className="flex flex-col items-center">
-            <h1 className="p-3 font-poppings text-5xl">Explorar Receitas</h1>
             <ReceitasMain />
         </div>
         </>

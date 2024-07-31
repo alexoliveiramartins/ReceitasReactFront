a240ae641b1b3acee7ebfc5bf9709993d6ecea1e
MarcoAntonioUFG <146884614+MarcoAntonioUFG@users.noreply.github.com>
2024-07-30 21:12:29 -0300

    savio receitas logo foda

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { NavLink } from "react-router-dom";
-import { SearchBar } from "./MainPage/SearchBar";
-import { SearchResultsList } from "./MainPage/SearchResultList";
+import { SearchBar } from "./MainPage/pesquisa/SearchBar";
+import { SearchResultsList } from "./MainPage/pesquisa/SearchResultList";
 import { useState } from "react";
 
 export default function Header({title}){
@@ -11,12 +11,15 @@ export default function Header({title}){
   return (
     <div>
       
-      <header className="h-20 // flex px-2 justify-between bg-green-800 font-poppings">
+      <header className="h-20 // flex px-2 justify-start bg-green-800 font-poppings">
         <NavLink to="/" className="my-auto">
           <img src="../src/imagens/logo.png" className="size-12"/>
         </NavLink>
-
-        <div className="w-1/2 flex flex-col my-auto mt-5 origin-top place-items-end justify z-10">
+        <NavLink to="/" className="my-auto">
+          <img src="../src/imagens/savioreceitas.png" className=""/>
+        </NavLink>
+        <div className="text-green-800">..................................</div>
+        <div className="w-1/2 flex flex-col my-auto mt-5 origin-top place-items-end  z-10">
           <SearchBar className="" setResults={setResults}/>
           {results && results.length > 0 && <SearchResultsList className="" results={results}/>}  
         </div>
@@ -24,8 +27,6 @@ export default function Header({title}){
         <nav className="flex items-center p-5">
           <ul className="flex gap-2.5 font-sans text-2xl">
             <li><NavLink to="/receitas">receitas</NavLink></li>
-            <li><NavLink to="/login">login</NavLink></li>
-            <li><NavLink to="/register">registro</NavLink></li>
           </ul>
         </nav>
      </header>
